cdf49977f02375af844c293a32e52d8e655a728d
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-06-01 13:42:26 -0300

    consertando prop do logo

--- src/global/Brand.tsx
+++ src/global/Brand.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/brand.module.scss";
 
 export default function Brand(props: { isBlue: boolean }) {
   return (
-    <div className={isBlue ? styles.blueLogo : styles.whiteLogo}>
+    <div className={props.isBlue ? styles.blueLogo : styles.whiteLogo}>
       <BubbleChartIcon fontSize='large' />
       <h2>ΟΡΙΟΝΜΠÓΡΔ</h2>
     </div>
