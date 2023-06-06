0484f4fc66eedf32fcba464e1f9619e1f2a682df
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-06-06 11:06:00 -0300

    adicionando clientes-teste

--- src/screens/Clients/ClientsTable.tsx
+++ src/screens/Clients/ClientsTable.tsx
@@ -1,5 +1,24 @@
 export default function ClientsTable(props: { data: [] }) {
     return (
-        <>{props.data.length < 1 ? <span>Nenhum cliente foi encontrado!</span> : <></>}</>
+        <>{props.data.length < 1 ? <span>Nenhum cliente foi encontrado!</span> : <>
+            <div style={{
+                display: "grid",
+                gridTemplateColumns: "33% 33% 13%"
+            }}>
+                <span>Nome</span>
+                <span>Endereço</span>
+                <span>Telefone</span>
+            </div>
+            {props.data.map(() => {
+                <div style={{
+                    display: "grid",
+                    gridTemplateColumns: "33% 33% 13%"
+                }}>
+                    <span>{props.data?.name!}</span>
+                    <span>{props.data?.address!}</span>
+                    <span>{props.data?.phoneNumber!}</span>
+                </div>
+            })}
+        </>}</>
     )
 }
