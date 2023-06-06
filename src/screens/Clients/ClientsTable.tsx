0039274cefbd38b76436f7f96a9b60f39c226b7b
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-06-06 11:08:24 -0300

    tipagem

--- src/screens/Clients/ClientsTable.tsx
+++ src/screens/Clients/ClientsTable.tsx
@@ -1,4 +1,4 @@
-export default function ClientsTable(props: { data: [] }) {
+export default function ClientsTable(props: { data: any[] }) {
     return (
         <>{props.data.length < 1 ? <span>Nenhum cliente foi encontrado!</span> : <>
             <div style={{
