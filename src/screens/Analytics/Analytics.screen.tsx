9fc39120b111f55eca5f35d451a99728eb78ab83
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-06-12 06:41:05 -0700

    tela de analytics

--- src/screens/Analytics/Analytics.screen.tsx
+++ src/screens/Analytics/Analytics.screen.tsx
@@ -1,5 +1,34 @@
 export default function Analytics(){
+    const RenderChart = (props: {
+        title: string;
+        children: React.ReactNode;
+    }) => {
+        return(<>
+            <h2>{props?.title}</h2>
+            <>{props?.children}</>
+        </>);
+    }
+
     return(
-        <></>
+        <>
+            <h2>Relatórios</h2>
+            <p>Resumo do perfil dos clientes que consomem seus produtos</p>
+            <RenderChart title="Gênero">
+                <></>
+            </RenderChart>
+            <RenderChart title="Faixa Etária">
+                <></>
+            </RenderChart>
+            <RenderChart title="Região">
+                <></>
+            </RenderChart>
+            <RenderChart title="Plataforma">
+                <></>
+            </RenderChart>
+            <RenderChart title="Avaliações">
+                <></>
+            </RenderChart>
+            <h2>Comentários</h2>
+        </>
     );
 }
