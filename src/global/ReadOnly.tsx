5e7b52a2216eb03c65a466b054f639c760d63a70
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-06-18 13:33:32 -0300

    alerta

--- src/global/ReadOnly.tsx
+++ src/global/ReadOnly.tsx
@@ -2,9 +2,13 @@ import {Alert, AlertTitle} from "@mui/material";
 
 export default function ReadOnly() {
     return (
+        <div style={{
+            maxWidth: "150px"
+        }}>
         <Alert severity="info">
             <AlertTitle>Modo somente leitura</AlertTitle>
             Você está no modo somente visualização porque sua conta não tem permissões de edição. Contate seu administrador.
         </Alert>
+        </div>
     )
 }
