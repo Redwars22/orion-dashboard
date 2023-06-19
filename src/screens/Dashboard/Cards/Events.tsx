20b16ed63ac16603ee2a71b3748131c610a2f619
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-06-19 11:15:36 -0300

    Update Events.tsx

--- src/screens/Dashboard/Cards/Events.tsx
+++ src/screens/Dashboard/Cards/Events.tsx
@@ -28,7 +28,7 @@ export default function EventsComponent(props: {
                 <DatePicker value={date} onChange={(value)=> {
                     setDate(value);    
                     window.alert(value)
-                }
+                }}
                 />
               </div>
             </LocalizationProvider>
