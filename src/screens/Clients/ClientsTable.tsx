4a91d790bbd3f607633f9092fb1025cac600aa60
Redwars22 <andrewpereira.megabyte@gmail.com>
2023-07-17 18:51:47 -0300

    melhorando responsivo das telas de clientes e dados financeiros

--- src/screens/Clients/ClientsTable.tsx
+++ src/screens/Clients/ClientsTable.tsx
@@ -1,52 +1,43 @@
 import { Button } from "@mui/material";
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import { IClients } from "../../global/types";
+import styles from "../../styles/layout.module.css";
 
 export default function ClientsTable(props: {
     data: IClients[]
 }) {
     return (
-        <>{props.data.length < 1 ? <span>Nenhum cliente foi encontrado!</span> : <>
-            <span>{props.data.length} cliente(s) encontrado(s)</span>
-            <div style={{
-                display: "grid",
-                gridTemplateColumns: "25% 15% 36% 18% 8%",
-                justifyItems: "start",
-                fontWeight: "bold",
-                borderBottomWidth: "1px",
-                borderBottomColor: "grey",
-                borderBottomStyle: "solid",
-            }}>
-                <span>Nome</span>
-                <span>CPF</span>
-                <span>Endereço</span>
-                <span>Telefone</span>
-                <span>Contato</span>
-            </div>
-            <>{props.data.map((item: IClients) =>
-                <div style={{
-                    display: "grid",
-                    gridTemplateColumns: "25% 15% 36% 18% 8%",
-                    justifyItems: "start",
-                    alignItems: "center"
-                }}>
-                    <span>{item?.name!}</span>
-                    <span>{item?.CPF!}</span>
-                    <span>{item?.address!}</span>
-                    <span>{item?.phoneNumber!}</span>
-                    <Button variant="text" onClick={() => {
-                        window.open(
-                            `https://wa.me/${item?.phoneNumber?.replace("(", "")
-                                .replace(")", "")
-                                .replace("-", "")
-                                .replace(" ", "")}`
-                        )
-                    }}>
-                        <WhatsAppIcon color="success" />
-                    </Button>
+        <>{props.data.length < 1
+            ? <span>Nenhum cliente foi encontrado!</span>
+            : <>
+                <span>{props.data.length} cliente(s) encontrado(s)</span>
+                <div className={styles.clientsTableHeader}>
+                    <span>Nome</span>
+                    <span>CPF</span>
+                    <span>Endereço</span>
+                    <span>Telefone</span>
+                    <span>Contato</span>
                 </div>
-            )}
-            </>
-        </>}</>
+                <>{props.data.map((item: IClients) =>
+                    <div className={styles.clientsTable}>
+                        <span>{item?.name!}</span>
+                        <span>{item?.CPF!}</span>
+                        <span>{item?.address!}</span>
+                        <span>{item?.phoneNumber!}</span>
+                        <Button variant="text" onClick={() => {
+                            window.open(
+                                `https://wa.me/${item?.phoneNumber?.replace("(", "")
+                                    .replace(")", "")
+                                    .replace("-", "")
+                                    .replace(" ", "")}`
+                            )
+                        }}>
+                            <WhatsAppIcon color="success" />
+                        </Button>
+                    </div>
+                )}
+                </>
+            </>}
+        </>
     )
 }
--- src/screens/Finances/Finances.table.tsx
+++ src/screens/Finances/Finances.table.tsx
@@ -1,44 +1,35 @@
 import { Button } from "@mui/material";
 import { IFinances } from "../../global/types";
 import SystemUpdateAltIcon from '@mui/icons-material/SystemUpdateAlt';
+import styles from "../../styles/layout.module.css";
 
-export default function FinancesTable(props: {data: IFinances[]}){
-    return(
-        <>{props.data.length < 1 ? <span>Não foram encontrados dados financeiros!</span> : <>
-            <div style={{
-                display: "grid",
-                gridTemplateColumns: "25% 25% 25% 25%",
-                justifyItems: "start",
-                fontWeight: "bold",
-                borderBottomWidth: "1px",
-                borderBottomColor: "grey",
-                borderBottomStyle: "solid",
-            }}>
-                <span>Ano</span>
-                <span>Mês</span>
-                <span>Saldo</span>
-                <span>Baixar Relatório</span>
-            </div>
-            <>{props.data.map((item: IFinances) =>
-                <div style={{
-                    display: "grid",
-                    gridTemplateColumns: "25% 25% 25% 25%",
-                    justifyItems: "start",
-                    alignItems: "center"
-                }}>
-                    <span>{item?.year!}</span>
-                    <span>{item?.month!}</span>
-                    <span style={{
-                        color: item?.amount < 0 ? "red" : "green",
-                        fontWeight: "900"
-                    }}>R$ {item?.amount!}</span>
-                    <Button variant="text" onClick={()=> window.open(item?.fileURL)}>
-                        <SystemUpdateAltIcon/>
-                        Baixar relatório do mês
-                    </Button>
+export default function FinancesTable(props: { data: IFinances[] }) {
+    return (
+        <>{props.data.length < 1
+            ? <span>Não foram encontrados dados financeiros!</span>
+            : <>
+                <div className={styles.financialDataTableHeader}>
+                    <span>Ano</span>
+                    <span>Mês</span>
+                    <span>Saldo</span>
+                    <span>Baixar Relatório</span>
                 </div>
-            )}
-            </>
-        </>}</>
+                <>{props.data.map((item: IFinances) =>
+                    <div className={styles.financialDataTable}>
+                        <span>{item?.year!}</span>
+                        <span>{item?.month!}</span>
+                        <span style={{
+                            color: item?.amount < 0 ? "red" : "green",
+                            fontWeight: "900"
+                        }}>R$ {item?.amount!}</span>
+                        <Button variant="text" onClick={() => window.open(item?.fileURL)}>
+                            <SystemUpdateAltIcon />
+                            Baixar relatório do mês
+                        </Button>
+                    </div>
+                )}
+                </>
+            </>}
+        </>
     )
 }
