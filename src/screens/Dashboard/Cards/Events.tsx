29f08618f6ba2867b4f87d028374f5d277ffc3ea
AndrewNation <andrewpereira.megabyte@gmail.com>
2023-08-11 20:29:21 -0300

    atualizando lógica

--- src/screens/Dashboard/Cards/Events.tsx
+++ src/screens/Dashboard/Cards/Events.tsx
@@ -2,55 +2,57 @@ import { TextField, Button, Stack, Card } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import {IEvent} from "../../../global/types";
+import { IEvent } from "../../../global/types";
 import React from "react";
 import dayjs from 'dayjs';
 
 export default function EventsComponent(props: {
-    data: IEvent[]
+  data: IEvent[]
 }) {
-    const [date, setDate] = React.useState("");
-    const [eventsInThatDate, setEventsInThatDate] = React.useState([]);
+  const [date, setDate] = React.useState("");
+  const [eventsInThatDate, setEventsInThatDate] = React.useState([]);
 
-    const events: IEvent[] = props.data;
+  const events: IEvent[] = props.data;
 
-    const eventsInThatDate = useCallback(()=>{
-      const eventsFiltered: IEvent[] = props.events.filter((i: IEvent) => i.date === date);
+  const updateFilteredEvents = useCallback(() => {
+    const eventsFiltered: IEvent[] = props.events.filter((i: IEvent) => i.date === date);
 
-      setEventsInThatDate(eventsFiltered);
-    }, [date])
-    
-    return (
-        <Card variant={"outlined"} sx={{
-            paddingLeft: "0.5rem",
-            paddingRight: "0.5rem",
-            paddingBottom: "0.5rem",
-            borderRadius: "10px",
-            flex: 1,
-            display: "flex",
-            flexDirection: "column",
-            gap: "0.5rem"
+    setEventsInThatDate(eventsFiltered);
+  }, [date])
+
+  const getEventCards = useCallback(() => <></>, [eventsInThatDate])
+
+  return (
+    <Card variant={"outlined"} sx={{
+      paddingLeft: "0.5rem",
+      paddingRight: "0.5rem",
+      paddingBottom: "0.5rem",
+      borderRadius: "10px",
+      flex: 1,
+      display: "flex",
+      flexDirection: "column",
+      gap: "0.5rem"
+    }}>
+      <h2>Eventos</h2>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <div style={{
+          maxWidth: "150px",
+          marginLeft: "auto",
+          marginRight: "auto"
         }}>
-            <h2>Eventos</h2>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <div style={{
-                maxWidth: "150px",
-                marginLeft: "auto",
-                marginRight: "auto"
-              }}>
-                <DatePicker value={date} onChange={(value)=> {
-                     setDate(value ? value : "");
+          <DatePicker value={date} onChange={(value) => {
+            setDate(value ? value : "");
 
-                     if (value) {
-                       const formattedDate = dayjs(value).format('MM-DD-YYYY'); // Format the date
-                       window.alert(formattedDate);
-                       console.log(formattedDate);
-                     }
-                }}
-                />
-              </div>
-            </LocalizationProvider>
-            {eventsFiltered.length === 0 && <span>Nenhuma data selecionada ou não há nenhum evento previsto para a data selecionada!</span>}
-        </Card>
-    )
+            if (value) {
+              const formattedDate = dayjs(value).format('MM-DD-YYYY'); // Format the date
+              updateFilteredEvents();
+            }
+          }}
+          />
+        </div>
+      </LocalizationProvider>
+      {eventsInThatDate.length === 0 ? <span>Nenhuma data selecionada ou não há nenhum evento previsto para a data selecionada!</span> :
+        <>{getEventCards()}</>}
+    </Card>
+  )
 }
